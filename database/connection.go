fe22217db90ee749e8564f1d51ff97e5d301af8b
Nattajak Grisiam <masinew@gmail.com>
2020-11-26 20:54:30 +0700

    database: initial database connection

---
+++ database/connection.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbUrl = `postgres://tddwulka:UlftGmzu4h5oRstCi_pZhlcMXNB6EVye@rosie.db.elephantsql.com:5432/tddwulka`
+	//dbUrl = os.Getenv("DB_URL")
+)
+
+var connection *sql.DB
+
+func Open() (*sql.DB, error) {
+	var err error
+	connection, err = sql.Open("postgres", dbUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	return connection, nil
+}
+
+func Connection() *sql.DB {
+
+	return connection
+}
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/masinew/gofinal/database"
+	"log"
+)
 
 func main() {
-	fmt.Println("customer service")
-	//run port ":2009"
+	conn, err := db.Open()
+	defer conn.Close()
+	if err != nil {
+		log.Fatalf("Database Connection error: %s", err)
+	}
+
+	r := gin.Default()
+	r.GET("/customer", handler)
+	r.Run(":2009")
+}
+
+func handler(c *gin.Context) {
+
 }
